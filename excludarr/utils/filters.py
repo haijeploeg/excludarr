b94a7e4557f81518edb6a74a0a0e390f04eb8175
Tailslide <5757188+Tailslide@users.noreply.github.com>
2022-01-15 14:14:13 -0700

    Remove logging of bad ids

--- excludarr/utils/filters.py
+++ excludarr/utils/filters.py
@@ -15,30 +15,16 @@ def bool2str(value):
 
 def get_tmdb_ids(external_ids):
     try:
-        all_tmdb_ids = [
-            x["external_id"]
-            for x in external_ids
-            if x["provider"] == "tmdb_latest" or x["provider"] == "tmdb"
-        ]
         tmdb_ids = [
-            int(x)
-            for x in all_tmdb_ids
-            if x.isnumeric()
+            int(x["external_id"])
+            for x in external_ids
+            if (x["provider"] == "tmdb_latest" or x["provider"] == "tmdb") and x["external_id"].isnumeric()
         ]
-        bad_tmdb_ids = [
-            x
-            for x in all_tmdb_ids
-            if not x.isnumeric()
-        ]
-        if (len(bad_tmdb_ids) > 0):
-            print("BAD TMDB IDS:",bad_tmdb_ids)
-
         tmdb_ids = list(set(tmdb_ids))
     except (KeyError, IndexError):
         tmdb_ids = []
 
     return tmdb_ids
-
 
 def get_imdb_ids(external_ids):
     try:
